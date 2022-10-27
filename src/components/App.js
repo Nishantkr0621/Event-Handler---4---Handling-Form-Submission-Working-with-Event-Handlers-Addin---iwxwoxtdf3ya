574725ed1bac8760865349f0d8fed7f090dc4d63
Nishant Kumar <nishantkr0621@gmail.com>
2022-10-27 09:48:10 +0000

    Update package-lock.json and App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,33 @@
-import React from 'react'
-import '../styles/App.css';
+import React from "react";
+import "../styles/App.css";
 const App = () => {
+  let count = 0;
+  const handleDoubleClick = (event) => {
+    ++count;
+    let singleClickTimer;
+    if (count === 1) {
+      singleClickTimer = setTimeout(() => {
+        count = 0;
+        console.log("I was not double clicked");
+      }, 400);
 
-  const handleDoubleClick = (event) =>{
-  
-  }
+      // console.log("I was not double clicked");
+    } else if (count === 2) {
+      clearTimeout(singleClickTimer);
+      count = 0;
+      console.log("I was double clicked");
+    }
+  };
   return (
     <div id="main">
-      <button id="dblclick-btn">Double click me</button>
+      <button
+        id="dblclick-btn"
+        onDoubleClick={(event) => handleDoubleClick(event)}
+      >
+        Double click me
+      </button>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
